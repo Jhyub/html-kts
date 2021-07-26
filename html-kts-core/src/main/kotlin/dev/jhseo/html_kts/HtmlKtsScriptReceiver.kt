d6591ad52abccd123ebc3b588f878933db682eb2
Janghyub Seo <seojanghyeob@gmail.com>
2021-07-26 13:47:36 +0900

    Make receiver inherit TagConsumer<String> via delegation

--- html-kts-core/src/main/kotlin/dev/jhseo/html_kts/HtmlKtsScriptReceiver.kt
+++ html-kts-core/src/main/kotlin/dev/jhseo/html_kts/HtmlKtsScriptReceiver.kt
@@ -1,7 +1,8 @@
 package dev.jhseo.html_kts
 
 import io.ktor.request.*
+import kotlinx.html.TagConsumer
+import kotlinx.html.stream.createHTML
 
-class HtmlKtsScriptReceiver(val request: ApplicationRequest?, val data: Map<String, Any?>) {
-
+class HtmlKtsScriptReceiver(val request: ApplicationRequest?, val data: Map<String, Any?>): TagConsumer<String> by createHTML() {
 }
