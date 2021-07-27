d080c874ef95cbf7f3e9658ffafeb87f21d24656
Janghyub Seo <seojanghyeob@gmail.com>
2021-07-27 12:18:59 +0900

    null -> exception

--- html-kts-core/src/main/kotlin/dev/jhseo/html_kts/compiler/Compile.kt
+++ html-kts-core/src/main/kotlin/dev/jhseo/html_kts/compiler/Compile.kt
@@ -5,7 +5,7 @@ import kotlin.script.experimental.api.valueOrNull
 import kotlin.script.experimental.jvm.impl.KJvmCompiledScript
 import kotlin.script.experimental.jvmhost.JvmScriptCompiler
 
-suspend fun compile(script: SourceCode): KJvmCompiledScript? {
+suspend fun compile(script: SourceCode): KJvmCompiledScript {
     val compileResult = JvmScriptCompiler()(script, HtmlKtsCompileConfig)
-    return compileResult.valueOrNull() as? KJvmCompiledScript
+    return compileResult.valueOrNull() as? KJvmCompiledScript ?: throw IllegalArgumentException("Script ${script.name} compilation failed")
 }
