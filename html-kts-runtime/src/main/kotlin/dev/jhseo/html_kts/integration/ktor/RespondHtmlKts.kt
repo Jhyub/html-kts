fb1cd5007cd0d9976568241f99bd7b6156969994
Janghyub Seo <seojanghyeob@gmail.com>
2021-07-28 11:45:24 +0900

    Add respondHtmlKts

---
+++ html-kts-runtime/src/main/kotlin/dev/jhseo/html_kts/integration/ktor/RespondHtmlKts.kt
@@ -0,0 +1,9 @@
+package dev.jhseo.html_kts.integration.ktor
+
+import dev.jhseo.html_kts.api.RenderOrder
+import io.ktor.application.*
+import io.ktor.http.*
+
+suspend fun ApplicationCall.respondHtmlKts(status: HttpStatusCode = HttpStatusCode.OK, renderOrder: RenderOrder) {
+
+}
