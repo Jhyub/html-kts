6bc8baa5bd2ad73782f98d8a1449cdda3a00e9d5
Janghyub Seo <seojanghyeob@gmail.com>
2021-07-28 11:10:41 +0900

    Pass instance-creating to call point

--- html-kts-core/src/main/kotlin/dev/jhseo/html_kts/compiler/Compile.kt
+++ html-kts-core/src/main/kotlin/dev/jhseo/html_kts/compiler/Compile.kt
@@ -5,7 +5,7 @@ import kotlin.script.experimental.api.valueOrNull
 import kotlin.script.experimental.jvm.impl.KJvmCompiledScript
 import kotlin.script.experimental.jvmhost.JvmScriptCompiler
 
-suspend fun compile(script: SourceCode): KJvmCompiledScript {
-    val compileResult = JvmScriptCompiler()(script, HtmlKtsCompileConfig)
+suspend fun JvmScriptCompiler.compile(script: SourceCode): KJvmCompiledScript {
+    val compileResult = invoke(script, HtmlKtsCompileConfig)
     return compileResult.valueOrNull() as? KJvmCompiledScript ?: throw IllegalArgumentException("Script ${script.name} compilation failed")
 }
--- html-kts-core/src/main/kotlin/dev/jhseo/html_kts/evaluator/Evaluate.kt
+++ html-kts-core/src/main/kotlin/dev/jhseo/html_kts/evaluator/Evaluate.kt
@@ -4,7 +4,6 @@ import dev.jhseo.html_kts.api.HtmlKtsScriptReceiver
 import kotlin.script.experimental.api.CompiledScript
 import kotlin.script.experimental.jvm.BasicJvmScriptEvaluator
 
-suspend fun evaluate(compiledScript: CompiledScript, receiver: HtmlKtsScriptReceiver) {
-    val evaluator = BasicJvmScriptEvaluator()
-    evaluator(compiledScript, HtmlKtsEvaluateConfig(receiver))
+suspend fun BasicJvmScriptEvaluator.evaluate(compiledScript: CompiledScript, receiver: HtmlKtsScriptReceiver) {
+    invoke(compiledScript, HtmlKtsEvaluateConfig(receiver))
 }
