1a7fdfd188405821f56f88aff5d431235082a6cc
Janghyub Seo <seojanghyeob@gmail.com>
2021-07-28 11:46:00 +0900

    Add findScript

---
+++ html-kts-core/src/main/kotlin/dev/jhseo/html_kts/FindScript.kt
@@ -0,0 +1,25 @@
+package dev.jhseo.html_kts
+
+import dev.jhseo.html_kts.compiler.compile
+import java.io.File
+import java.lang.IllegalArgumentException
+import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.impl.KJvmCompiledScript
+import kotlin.script.experimental.jvmhost.JvmScriptCompiler
+
+internal suspend fun findCompiledScript(name: String): KJvmCompiledScript? {
+    val source = {}::class.java.getResource("/html/$name.html.kts.compiled")
+    val sourceFile = source?.let { File(it.toURI()) }
+    return sourceFile?.let { KJvmCompiledScript.fromFile(it) }
+}
+
+internal suspend fun JvmScriptCompiler.findAndCompileRawScript(name: String): KJvmCompiledScript? {
+    val source = {}::class.java.getResource("/html/$name.html.kts")
+    val sourceFile = source?.let { File(it.toURI()) }
+    return sourceFile?.toScriptSource()?.let { this@findAndCompileRawScript.compile(it) }
+}
+
+suspend fun JvmScriptCompiler.findScript(name: String): KJvmCompiledScript {
+    return findCompiledScript(name) ?: findAndCompileRawScript(name)
+    ?: throw IllegalArgumentException("Can't find script $name. Candidates were: [\"/html/$name.html.kts.compiled\", \"/html/$name.html.kts\"]")
+}
